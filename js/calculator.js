f617498fb488d9ed69b82a67676df0a62bf3cc96
Ali-Weli <59826486+Ali-Weli@users.noreply.github.com>
2020-06-27 19:06:26 +0500

    Add files via upload

---
+++ js/calculator.js
@@ -0,0 +1,10 @@
+// simple calculator, just adding two numbers
+// window.promp(""); is taking input from user
+
+var num1 = window.prompt("Num1: ");
+var num2 = window.prompt("Num2: ");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+alert(num1 + num2);
